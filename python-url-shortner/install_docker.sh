9ab2e129e17b1cea9e7ba137a67327d0ed8d9368
Adam Listek <adam@listekconsulting.com>
2021-10-26 23:53:01 -0500

    Update

--- python-url-shortner/install_docker.sh
+++ python-url-shortner/install_docker.sh
@@ -1,26 +1,24 @@
-#!*/bin/bash*
+#!/bin/bash
 
-# update VM apt repository
+# Update container Linux image APT repository data
 apt-get update
 
-# install docker-dependent packages
+# Install Docker required packages
 apt-get -y install apt-transport-https ca-certificates lsb-release
 
-# add docker GPG key
+# Add the Docker GPG key to trust the Docker APT repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# update VM apt repository after adding docker
+# Update container Linux image APT repository data again after adding the Docker repository
 apt-get update
 
-# install docker community edition and cli
+# Install the Docker community edition and cli
 apt-get -y install docker-ce docker-ce-cli containerd.io
 
-# download docker-compose executable file
+# Download docker-compose executable file, replace the 1.29.2 listed below with the latest version you intend to use.
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-# change permission of docker-compose to become executable
+# Change permission of docker-compose to become executable
 chmod +x /usr/local/bin/docker-compose
